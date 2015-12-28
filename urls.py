be39f70f8bf065686779c8bf758dd5cdfa370f26
exegoist <exegoist@icloud.com>
2015-12-28 12:24:21 +0200

    url patterns updated for 1.10 ready

--- urls.py
+++ urls.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
 
-#urlpatterns = patterns('',
-#    url(r'^files/([0-9]{4})/([0-9]{2})/$', 'filemanager.views.list_requested'),
-#)
-
-urlpatterns = [
-        url(r'^$', 'filemanager.views.list_current', name='list_current'),
-        url(r'^scan/$', 'filemanager.views.scan_for_files', name='scan_for_files'),
-        url(r'^(?P<year>[0-9]{4})/$', 'filemanager.views.list_by_date', name='list_by_date'),
-        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', 'filemanager.views.list_by_date', name='list_by_date'),
-        url(r'^Video_SMI/$', 'filemanager.views.list_by_path', {'path': 'Video_SMI'}, name='list_by_path'),
-        url(r'^delete/(?P<id>\d+)$', 'filemanager.views.delete_file'),
-]
+urlpatterns = patterns( 'filemanager.views',
+        url(r'^$', 'list_current', name='list_current'),
+        url(r'^scan/$', 'scan_for_files', name='scan_for_files'),
+        url(r'^(?P<year>[0-9]{4})/$', 'list_by_date', name='list_by_date'),
+        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', 'list_by_date', name='list_by_date'),
+        url(r'^Video_SMI/$', 'list_by_path', {'path': 'Video_SMI'}, name='list_by_path'),
+        url(r'^delete/(?P<id>\d+)$', 'delete_file'),
+)
