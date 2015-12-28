83318581a85e9910c2fcc3430001e41025abc817
exegoist <exegoist@icloud.com>
2015-12-28 16:00:38 +0200

    urls listed

--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
+from filemanager import views as file_views
 
-urlpatterns = patterns( 'filemanager.views',
-        url(r'^$', list_current),
-        url(r'^scan/$', scan_for_files),
-        url(r'^(?P<year>[0-9]{4})/$', list_by_date),
-        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', list_by_date),
-        url(r'^Video_SMI/$', list_by_path, {'path': 'Video_SMI'}),
-        url(r'^delete/(?P<id>\d+)$', delete_file),
-)
+urlpatterns = [
+        url(r'^$', file_views.list_current),
+        url(r'^scan/$', file_views.scan_for_files),
+        url(r'^(?P<year>[0-9]{4})/$', file_views.list_by_date),
+        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', file_views.list_by_date),
+        url(r'^Video_SMI/$', file_views.list_by_path, {'path': 'Video_SMI'}),
+        url(r'^delete/(?P<id>\d+)$', file_views.delete_file),
+]
