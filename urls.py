733f840084aaf8cc84210bf9f33856017d82962d
exegoist <exegoist@icloud.com>
2015-12-28 15:41:24 +0200

    urls names deleted

--- urls.py
+++ urls.py
@@ -2,10 +2,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns( 'filemanager.views',
-        url(r'^$', list_current, name='list_current'),
-        url(r'^scan/$', scan_for_files, name='scan_for_files'),
-        url(r'^(?P<year>[0-9]{4})/$', list_by_date, name='list_by_date'),
-        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', list_by_date, name='list_by_date'),
-        url(r'^Video_SMI/$', list_by_path, {'path': 'Video_SMI'}, name='list_by_path'),
+        url(r'^$', list_current),
+        url(r'^scan/$', scan_for_files),
+        url(r'^(?P<year>[0-9]{4})/$', list_by_date),
+        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', list_by_date),
+        url(r'^Video_SMI/$', list_by_path, {'path': 'Video_SMI'}),
         url(r'^delete/(?P<id>\d+)$', delete_file),
 )
